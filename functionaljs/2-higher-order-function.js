c0a6544ab8aabdc09f16bc0d74e4b920b047da85
mrmnzz <manish79kr@gmail.com>
2019-06-21 14:48:22 +0530

    added higher order function

---
+++ functionaljs/2-higher-order-function.js
@@ -0,0 +1,8 @@
+function repeat(operation, num) {
+      // SOLUTION GOES HERE
+      for(var i=0; i<num; i++)
+        operation();
+    }
+
+    // Do not remove the line below
+    module.exports = repeat
